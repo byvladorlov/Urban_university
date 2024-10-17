0d6515b2c7c0560424fa7535ee0b010bbca9c76d
byvladorlov <vlad.orlov.9874@gmail.com>
2024-10-18 01:02:47 +0200

    3 DZ

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hour_count = 1.5
+training_courses = 'Python'
+time_task = hour_count/homework_count
+print('Курс:', training_courses, ', всего задач:', homework_count, ', затрачено часов:', hour_count, ', среднее время выполнения', time_task)
