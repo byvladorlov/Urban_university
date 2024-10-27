b74c6b4d6fa900f9429f90e768405e628a5b3900
byvladorlov <vlad.orlov.9874@gmail.com>
2024-10-27 21:40:35 +0200

    3 DZ

--- variables.py
+++ variables.py
@@ -2,4 +2,4 @@ homework_count = 12
 hour_count = 1.5
 training_courses = 'Python'
 time_task = hour_count/homework_count
-print('Курс:', training_courses, ', всего задач:', homework_count, ', затрачено часов:', hour_count, ', среднее время выполнения', time_task)
+print('Курс:', training_courses, ', всего задач:', homework_count, ', затрачено часов:', hour_count, ', среднее время выполнения', time_task, 'часа')
