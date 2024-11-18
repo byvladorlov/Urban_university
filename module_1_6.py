7a680e484d1467169232d36aa14a1b01dbcc50b4
BYVLADORLOV <vlad.orlov.9874@gmail.com>
2024-11-18 23:04:49 +0200

    7 DZ

---
+++ module_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Vlad': 1998, 'Sasha': 1997, 'Nikita': 1991, 'Olga': 1977, 'Vika': 2005}
+print('Dict: ', my_dict)
+print('Existing value: ', my_dict.get('Sasha'))
+print('Not existing value: ', my_dict.get('Oleg','None'))
+my_dict.update({'Oleg': 1965, 'Katya': 1985})
+print('Deleted value: ', my_dict.pop('Nikita'))
+print('Modified dictionary: ', my_dict)
+print('\n')
+my_set = {'Apple', 'Samsung', 33, 1, 'Lenovo', 17, 'Samsung', 17, 3, 'Apple',2,33,'Apple',1}
+print('Set:', my_set)
+my_set.update({'Xiaomi', 303})
+my_set.remove('Lenovo')
+print('Modified set: ', my_set)
