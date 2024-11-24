52b7336f9a307b3ae2b3afa93f0d0db3ac36ebdb
byvladorlov <vlad.orlov.9874@gmail.com>
2024-11-24 10:06:52 +0200

    13 DZ

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,12 +1,9 @@
 import random
-#num_1 = random.randint(3,20)
-num_1 = 9
+num_1 = random.randint(3,20)
 print(num_1)
 for i in range(1, num_1):
     #print(i, end=' ')
     for j in range(2,num_1):
-        if num_1 % (i + j) == 0:
-            print(i,j, end=' ')
-            continue
-        if i == j:
-            break
+        if num_1 % (i + j) == 0 and i < j:
+            print(f'{i}{j}', end='')
+
