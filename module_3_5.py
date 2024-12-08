e5979fe7b1506948c5d255215f9aa7920775473d
byvladorlov <vlad.orlov.9874@gmail.com>
2024-12-09 00:47:47 +0200

    Update module_3_5.py
    18 DZ

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,14 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) <= 1:
-        return first
-    else:
+    if (int(str_number[len(str_number)-1])) == 0: #проверка окончания числа на 0
+        str_number = str_number.strip('0') #убираем 0 в конце
+    if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
 
-result = get_multiplied_digits(23435)
+result = get_multiplied_digits(40203)
 print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
