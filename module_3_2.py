dc669c2adcc76a7ba3e4952e9d2610a7cc459806
byvladorlov <vlad.orlov.9874@gmail.com>
2024-12-08 23:50:44 +0200

    Update module_3_2.py
    15 DZ

--- module_3_2.py
+++ module_3_2.py
@@ -1,13 +1,15 @@
 def send_email(message, recipient,*, sender = "university.help@gmail.com"):
-    if recipient in sender:
-        print("Нельзя отправить письмо самому себе!")
-        return
     if (recipient.find("@") == -1 or sender.find("@") == -1 or
             recipient.endswith(('.com', '.ru', '.net')) == False
             or sender.endswith(('.com','.ru', '.net')) == False):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         return
-    if sender in "university.help@gmail.com":
+
+    if recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+    if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
@@ -16,5 +18,3 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
-
-
