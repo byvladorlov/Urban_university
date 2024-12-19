fb0a29bcf69787caeaddb988e5c96cc98e398057
BYVLADORLOV <vlad.orlov.9874@gmail.com>
2024-12-19 10:30:11 +0200

    22 DZ

---
+++ module_5_1.py
@@ -0,0 +1,24 @@
+from xml.sax.handler import property_interning_dict
+
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1:
+            return print("Такого этажа не существует")
+        for i in range(1, new_floor+1):
+            if new_floor > self.number_of_floors:
+                return print("Такого этажа не существует")
+            else:
+                print(i)
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+
+
